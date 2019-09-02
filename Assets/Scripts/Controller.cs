2d60473a4bafd40faf6260f9281261b57c70c6e0
insanefury <justdarsan@gmail.com>
2019-09-02 11:15:07 -0300

    Added Basic Grid Spawn

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject[] tiles;
+    GameObject[,] grid;
+
+    Model model;
+    View view;
+
+    [SerializeField]
+    int sizeX;
+    [SerializeField]
+    int sizeY;
+    [SerializeField]
+    Vector2 offset;
+
+    void Start()
+    {
+        GridInit();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void GridInit()
+    {
+        grid = new GameObject[sizeX, sizeY];
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                GameObject go =
+                    Instantiate(tiles[Random.Range(0, tiles.Length)], new Vector2(i + offset.x, j + offset.y),
+                    Quaternion.identity);
+                go.transform.SetParent(gameObject.transform);
+                go.name = go.name.Replace(" (clone)", " ").Trim();
+                grid[i, j] = go;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/View.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour
+{
+
+    public GameObject[] tiles;
+    GameObject [,] grid;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+   
+}
